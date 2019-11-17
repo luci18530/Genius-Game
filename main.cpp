c8242cf73791b324d518b17f65f855a7f25b5f73
Luciano Pereira <lu12345pe@gmail.com>
2019-11-17 02:17:56 +0000

    final version

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 #include <ctime>
 #include <unistd.h>
 using namespace std;
-#define tamfacil 6
-#define tammedio 12
-#define tamdificil 18
+#define tamfacil 5
+#define tammedio 10
+#define tamdificil 15
 
 void clear (){
   int c;
@@ -15,17 +15,58 @@ void clear (){
   }
 }
 
+void perdeu(){
+  cout << "\n--------------------------\n";
+  cout << "Sinto muito, você errou !!!\n";
+  cout << "Pressione qualquer tecla para voltar ao inicio\n";
+  cin.get();
+  cin.ignore();
+  clear();
+}
+
+void ganhou(){
+  cout << "\n--------------------------\n";
+  cout << "Parabéns, você acertou !!!\n";
+  cout << "Pressione qualquer tecla para voltar ao inicio\n";
+  cin.get();
+  cin.ignore();
+  clear();
+}
+
+void apresentadecora(){
+  clear();
+  cout << "Tente decorar esses numeros, pressione qualquer tecla para continuar\n";
+  cin.get();
+  cin.ignore();
+}
+
+void fim(bool error){
+  if (error == true){
+          perdeu();
+        }
+
+        if (error == false){
+          ganhou();
+        }
+
+      }
+
+
 int main() {
   
   int i;
   int opcinicial = 0;
   int opcdifi = 0;
   int sequenciafacil[tamfacil];
+  int respostafacil[tamfacil];
   int sequenciamedia[tammedio];
+  int respostamedia[tamfacil];
   int sequenciadificil[tamdificil];
+  int respostadificil[tamdificil];
   srand (time(NULL));
+  
   while (opcinicial != 2){
-
+    bool error = false;
   cout << " -------------\n";
   cout << ": GENIUS GAME :\n";
   cout << " -------------\n";
@@ -45,37 +86,104 @@ int main() {
       cin >> opcdifi;
 
       if (opcdifi == 1){
-
-        cin.get();
-        cin.ignore();
+        apresentadecora();
         
         for (i=0;i<tamfacil; i++){
           sequenciafacil[i] = rand()%4+1;
           
         }
-
+        // apresentar a sequencia aleatoria
         for (i=0; i<tamfacil; i++){
           clear();
-          cout << sequenciafacil[i] << "\n";
+          cout << "Numero [" << i+1 << "]: "<< sequenciafacil[i] << "\n";
           usleep(1000000);
           
         }
 
+        clear();
+        cout << "Agora digite de um em um a sequencia numerica apresentada:\n\n";
+        // digitar resposta
+        for (i=0;i<tamfacil; i++){
+          cout << "Numero [" << i+1 << "]: " ;
+          cin >> respostafacil[i];
+          
+        }
+
+        // verificar erros
+        for (i=0;i<tamfacil; i++){
+          if (respostafacil[i] != sequenciafacil[i]){
+            error = true;
+          }
+        }
+
+        fim(error);
+
       }
+
       if (opcdifi == 2){
+        apresentadecora();
         for (i=0;i<tammedio; i++){
           sequenciamedia[i] = rand()%6+1;
         }
-      }
+          for (i=0; i<tammedio; i++){
+          clear();
+          cout << "Numero [" << i+1 << "]: "<< sequenciamedia[i] << "\n";
+          usleep(750000);
+          
+        }
+
+        clear();
+        cout << "Agora digite de um em um a sequencia numerica apresentada:\n\n";
+        // digitar resposta
+        for (i=0;i<tammedio; i++){
+          cout << "Numero [" << i+1 << "]: " ;
+          cin >> respostamedia[i];
+          
+        }
+
+        // verificar erros
+        for (i=0;i<tammedio; i++){
+          if (respostamedia[i] != sequenciamedia[i]){
+            error = true;
+          }
+          
+        }
+
+        fim(error);
+
+        }
+
       if (opcdifi == 3){
         for (i=0;i<tamdificil; i++){
           sequenciadificil[i] = rand()%8+1;
         }
-      }
 
-      
+        for (i=0; i<tamdificil; i++){
+          clear();
+          cout << "Numero [" << i+1 << "]: "<< sequenciadificil[i] << "\n";
+          usleep(500000);
+                
+        }
+
+        clear();
+        cout << "Agora digite de um em um a sequencia numerica apresentada:\n\n";
+        // digitar resposta
+        for (i=0;i<tamdificil; i++){
+          cout << "Numero [" << i+1 << "]: " ;
+          cin >> respostadificil[i];
+          
+        }
+
+        // verificar erros
+        for (i=0;i<tamdificil; i++){
+          if (respostadificil[i] != sequenciadificil[i]){
+            error = true;
+          }
+        }
+
+        fim(error);
+
+      }      
     }
-
   }
 }
-
